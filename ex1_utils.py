ba24dd2d77ab224acb6c80ff79478e291e67c9e3
Ido Bar <ido1403@gmail.com>
2022-03-31 21:14:29 +0300

    finished quantization

--- ex1_utils.py
+++ ex1_utils.py
@@ -33,8 +33,9 @@ def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
     :param representation: GRAY_SCALE or RGB
     :return: The image object
     """
-    img = cv2.imread(filename, )
-
+    img = cv2.imread(filename)
+    # check whether the wanted representation is in
+    # RGB or GRAYSCALE
     if representation == 1:
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         norm_img = img_gray / 255
@@ -52,6 +53,7 @@ def imDisplay(filename: str, representation: int):
     :param representation: GRAY_SCALE or RGB
     :return: None
     """
+    # using the function above to successfully read the img
     img = imReadAndConvert(filename, representation)
     if representation == 1:
         plt.gray()
@@ -65,9 +67,11 @@ def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
     :param imgRGB: An Image in RGB
     :return: A YIQ in image color space
     """
+    # matrix for the transformation
     rgb_to_yiq = np.array([[0.299, 0.587, 0.114],
                            [0.596, -0.275, -0.321],
                            [0.212, -0.523, 0.311]])
+    # matrix multiplication
     yiq_img = np.dot(imgRGB, rgb_to_yiq.transpose())
     return yiq_img
 
@@ -78,9 +82,11 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
     :param imgYIQ: An Image in YIQ
     :return: A RGB in image color space
     """
+    # matrix for the transformation
     yiq_to_rgb = np.array([[0.299, 0.587, 0.114],
                            [0.596, -0.275, -0.321],
                            [0.212, -0.523, 0.311]])
+    # matrix multiplication but with the inverse matrix
     rgb_img = np.dot(imgYIQ, np.linalg.inv(yiq_to_rgb).transpose())
     return rgb_img
 
@@ -99,15 +105,19 @@ def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarra
     # normalize back to [0,255]
     norm_image = cv2.normalize(imgOrig, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
     norm_image = norm_image.astype(np.uint8)
+    # calculate histogram for the old img
     histogram = np.histogram(norm_image.ravel(), bins=256)[0]
     cum_sum = np.cumsum(histogram)
     lut = (cum_sum / max(cum_sum)) * 255
     new_img = np.zeros_like(imgOrig)
+    # calculate the lut
     for i in range(256):
         new_img[norm_image == i] = lut[i]
+    # flatten the new img in order to make histogram
     new_histogram = np.histogram(new_img.ravel(), bins=256)[0]
     if is_rgb == True:
-        yiq_img[:, :, 0] = new_img/255.0
+        # if the img was RGB then transfer it back
+        yiq_img[:, :, 0] = new_img / 255.0
         new_img = transformYIQ2RGB(yiq_img)
     else:
         new_img /= 255.0
@@ -122,4 +132,52 @@ def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarr
         :param nIter: Number of optimization loops
         :return: (List[qImage_i],List[error_i])
     """
+    is_rgb = False
+    # check if the img is in RGB colorspace
+    if len(imOrig.shape) == 3:
+        is_rgb = True
+        yiq_img = transformRGB2YIQ(imOrig)  # transform to YIQ
+        imOrig = yiq_img[:, :, 0]  # take only the Y from the YIQ
+    images, errors = quantize_one_dim(imOrig, nQuant, nIter)
+    if is_rgb:
+        new_images = []
+        for img in images:
+            # set back the new Y
+            yiq_img[:, :, 0] = img
+            curr_img = transformYIQ2RGB(yiq_img)
+            new_images.append(curr_img)
+        return new_images, errors
+    else:
+        return images, errors
 
+
+def quantize_one_dim(imgOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
+    mses = []  # the error for each iteration
+    new_images = []
+    img_hist = np.histogram((imgOrig.ravel() * 255), bins=256)[0]
+    boarder_z = []  # the boarders
+    # select the boundaries for the start of the iterations
+    for i in range(nQuant + 1):
+        boarder_z.append(int((255 / nQuant) * i))
+    for i in range(nIter):
+        averages = []
+        imgs = np.zeros_like(imgOrig)
+        for j in range(nQuant):
+            # get the number of pixels in the histogram between j and j+1
+            pixels_in_range = img_hist[boarder_z[j]:boarder_z[j + 1]]
+            index = np.arange(int(boarder_z[j]), int(boarder_z[j + 1]))
+            # get the average of all the pixels that between the boundaries
+            avg = (index * pixels_in_range).sum() / (pixels_in_range.sum()).astype(int)
+            averages.append(avg)
+        # set every pixel inside boundaries as the average
+        for a_v_g in range(len(averages)):
+            imgs[imgOrig > boarder_z[a_v_g] / 255] = averages[a_v_g]
+        # set new and better boundaries according to the averages in every boundry
+        for boarder in range(1, len(boarder_z) - 1):
+            boarder_z[boarder] = int((averages[boarder - 1] + averages[boarder]) / 2)
+        # calculate the mse between the old and the new img
+        curr_ms_error = np.sqrt((imgOrig * 255 - imgs) ** 2).mean()
+        mses.append(curr_ms_error)
+        new_images.append(imgs / 255)
+
+    return new_images, mses
