76a49a39ae33ff75bc733d867c74d69e5bca49bd
Ido Bar <ido1403@gmail.com>
2022-03-31 22:36:44 +0300

    added exceptions

--- ex1_utils.py
+++ ex1_utils.py
@@ -33,6 +33,12 @@ def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
     :param representation: GRAY_SCALE or RGB
     :return: The image object
     """
+    if filename is None:
+        print("Could not open the image")
+        exit(0)
+    if representation != 1 and representation != 2:
+        print("Not valid representation")
+        exit(0)
     img = cv2.imread(filename)
     # check whether the wanted representation is in
     # RGB or GRAYSCALE
@@ -53,6 +59,12 @@ def imDisplay(filename: str, representation: int):
     :param representation: GRAY_SCALE or RGB
     :return: None
     """
+    if filename is None:
+        print("Could not open the image")
+        exit(0)
+    if representation != 1 and representation != 2:
+        print("Not valid representation")
+        exit(0)
     # using the function above to successfully read the img
     img = imReadAndConvert(filename, representation)
     if representation == 1:
@@ -67,6 +79,9 @@ def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
     :param imgRGB: An Image in RGB
     :return: A YIQ in image color space
     """
+    if imgRGB is None:
+        print("Not valid ndarray")
+        exit(0)
     # matrix for the transformation
     rgb_to_yiq = np.array([[0.299, 0.587, 0.114],
                            [0.596, -0.275, -0.321],
@@ -82,6 +97,9 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
     :param imgYIQ: An Image in YIQ
     :return: A RGB in image color space
     """
+    if imgYIQ is None:
+        print("Not valid ndarray")
+        exit(0)
     # matrix for the transformation
     yiq_to_rgb = np.array([[0.299, 0.587, 0.114],
                            [0.596, -0.275, -0.321],
@@ -97,31 +115,37 @@ def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarra
         :param imgOrig: Original Histogram
         :ret
     """
+    if imgOrig is None:
+        print("Could not open the image")
+        exit(0)
     is_rgb = False
-    if len(imgOrig.shape) == 3:
-        is_rgb = True
-        yiq_img = transformRGB2YIQ(imgOrig)
-        imgOrig = yiq_img[:, :, 0]
-    # normalize back to [0,255]
-    norm_image = cv2.normalize(imgOrig, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    norm_image = norm_image.astype(np.uint8)
-    # calculate histogram for the old img
-    histogram = np.histogram(norm_image.ravel(), bins=256)[0]
-    cum_sum = np.cumsum(histogram)
-    lut = (cum_sum / max(cum_sum)) * 255
-    new_img = np.zeros_like(imgOrig)
-    # calculate the lut
-    for i in range(256):
-        new_img[norm_image == i] = lut[i]
-    # flatten the new img in order to make histogram
-    new_histogram = np.histogram(new_img.ravel(), bins=256)[0]
-    if is_rgb == True:
-        # if the img was RGB then transfer it back
-        yiq_img[:, :, 0] = new_img / 255.0
-        new_img = transformYIQ2RGB(yiq_img)
-    else:
-        new_img /= 255.0
-    return new_img, histogram, new_histogram
+    try:
+        if len(imgOrig.shape) == 3:
+            is_rgb = True
+            yiq_img = transformRGB2YIQ(imgOrig)
+            imgOrig = yiq_img[:, :, 0]
+        # normalize back to [0,255]
+        norm_image = cv2.normalize(imgOrig, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        norm_image = norm_image.astype(np.uint8)
+        # calculate histogram for the old img
+        histogram = np.histogram(norm_image.ravel(), bins=256)[0]
+        cum_sum = np.cumsum(histogram)
+        lut = (cum_sum / max(cum_sum)) * 255
+        new_img = np.zeros_like(imgOrig)
+        # calculate the lut
+        for i in range(256):
+            new_img[norm_image == i] = lut[i]
+        # flatten the new img in order to make histogram
+        new_histogram = np.histogram(new_img.ravel(), bins=256)[0]
+        if is_rgb == True:
+            # if the img was RGB then transfer it back
+            yiq_img[:, :, 0] = new_img / 255.0
+            new_img = transformYIQ2RGB(yiq_img)
+        else:
+            new_img /= 255.0
+        return new_img, histogram, new_histogram
+    except:
+        exit(0)
 
 
 def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
@@ -132,64 +156,73 @@ def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarr
         :param nIter: Number of optimization loops
         :return: (List[qImage_i],List[error_i])
     """
+    if imOrig is None:
+        print("Could not open the image")
+        exit(0)
     is_rgb = False
-    # check if the img is in RGB colorspace
-    if len(imOrig.shape) == 3:
-        is_rgb = True
-        yiq_img = transformRGB2YIQ(imOrig)  # transform to YIQ
-        imOrig = yiq_img[:, :, 0]  # take only the Y from the YIQ
-    images, errors = quantize_one_dim(imOrig, nQuant, nIter)
-    if is_rgb:
-        new_images = []
-        for img in images:
-            # set back the new Y
-            yiq_img[:, :, 0] = img
-            curr_img = transformYIQ2RGB(yiq_img)
-            new_images.append(curr_img)
-        return new_images, errors
-    else:
-        return images, errors
+    try:
+        # check if the img is in RGB colorspace
+        if len(imOrig.shape) == 3:
+            is_rgb = True
+            yiq_img = transformRGB2YIQ(imOrig)  # transform to YIQ
+            imOrig = yiq_img[:, :, 0]  # take only the Y from the YIQ
+        images, errors = quantize_one_dim(imOrig, nQuant, nIter)
+        if is_rgb:
+            new_images = []
+            for img in images:
+                # set back the new Y
+                yiq_img[:, :, 0] = img
+                curr_img = transformYIQ2RGB(yiq_img)
+                new_images.append(curr_img)
+            return new_images, errors
+        else:
+            return images, errors
+    except:
+        exit(0)
 
 
 def quantize_one_dim(imgOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
-    mses = []  # the error for each iteration
-    new_images = []
-    img_hist = np.histogram((imgOrig.ravel() * 255), bins=256)[0]
-    boarder_z = []  # the boarders
-    # select the boundaries for the start of the iterations
-    cum_sum = np.cumsum(img_hist)
-    boarder_z = [0]  # adding 0 as first boarder
-    num_of_pixels = img_hist.sum() / nQuant  # calculate number of pixels that should be between two boarders
-    curr_sum = 0
-    # select the best boundaries for the start of the iterations
-    for i in range(0, 256):
-        # check if passed the amount of pixels for the current boundary
-        curr_sum += img_hist[i]
-        if curr_sum >= num_of_pixels:
-            boarder_z.append(i + 1)
-            curr_sum = 0
-    # add 256 as the last boundary
-    if 255 not in boarder_z:
-        boarder_z.append(255)
-    for i in range(nIter):
-        averages = []
-        imgs = np.zeros_like(imgOrig)
-        for j in range(nQuant):
-            # get the number of pixels in the histogram between j and j+1
-            pixels_in_range = img_hist[boarder_z[j]:boarder_z[j + 1]]
-            index = np.arange(int(boarder_z[j]), int(boarder_z[j + 1]))
-            # get the average of all the pixels that between the boundaries
-            avg = (index * pixels_in_range).sum() / (pixels_in_range.sum()).astype(int)
-            averages.append(avg)
-        # set every pixel inside boundaries as the average
-        for a_v_g in range(len(averages)):
-            imgs[imgOrig > boarder_z[a_v_g] / 255] = averages[a_v_g]
-        # set new and better boundaries according to the averages in every boundary
-        for boarder in range(1, len(boarder_z) - 1):
-            boarder_z[boarder] = int((averages[boarder - 1] + averages[boarder]) / 2)
-        # calculate the mse between the old and the new img
-        curr_ms_error = np.sqrt((imgOrig * 255 - imgs) ** 2).mean()
-        mses.append(curr_ms_error)
-        new_images.append(imgs / 255)
+    try:
+        mses = []  # the error for each iteration
+        new_images = []
+        img_hist = np.histogram((imgOrig.ravel() * 255), bins=256)[0]
+        boarder_z = []  # the boarders
+        # select the boundaries for the start of the iterations
+        cum_sum = np.cumsum(img_hist)
+        boarder_z = [0]  # adding 0 as first boarder
+        num_of_pixels = img_hist.sum() / nQuant  # calculate number of pixels that should be between two boarders
+        curr_sum = 0
+        # select the best boundaries for the start of the iterations
+        for i in range(0, 256):
+            # check if passed the amount of pixels for the current boundary
+            curr_sum += img_hist[i]
+            if curr_sum >= num_of_pixels:
+                boarder_z.append(i + 1)
+                curr_sum = 0
+        # add 256 as the last boundary
+        if 255 not in boarder_z:
+            boarder_z.append(255)
+        for i in range(nIter):
+            averages = []
+            imgs = np.zeros_like(imgOrig)
+            for j in range(nQuant):
+                # get the number of pixels in the histogram between j and j+1
+                pixels_in_range = img_hist[boarder_z[j]:boarder_z[j + 1]]
+                index = np.arange(int(boarder_z[j]), int(boarder_z[j + 1]))
+                # get the average of all the pixels that between the boundaries
+                avg = (index * pixels_in_range).sum() / (pixels_in_range.sum()).astype(int)
+                averages.append(avg)
+            # set every pixel inside boundaries as the average
+            for a_v_g in range(len(averages)):
+                imgs[imgOrig > boarder_z[a_v_g] / 255] = averages[a_v_g]
+            # set new and better boundaries according to the averages in every boundary
+            for boarder in range(1, len(boarder_z) - 1):
+                boarder_z[boarder] = int((averages[boarder - 1] + averages[boarder]) / 2)
+            # calculate the mse between the old and the new img
+            curr_ms_error = np.sqrt((imgOrig * 255 - imgs) ** 2).mean()
+            mses.append(curr_ms_error)
+            new_images.append(imgs / 255)
 
-    return new_images, mses
+        return new_images, mses
+    except:
+        exit(0)
